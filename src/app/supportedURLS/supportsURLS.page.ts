f7e324117c5f6b3ce69eab08c5a58f4e3f2d5276
Segi <shovav@cryptolab.net>
2021-10-09 20:43:51 -0700

    Finish implementing supported URLs

--- src/app/supportedURLS/supportsURLS.page.ts
+++ src/app/supportedURLS/supportsURLS.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../core/data.service';
 
 @Component({
     selector: 'app-supportedURLS',
@@ -6,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['supportsURLS.page.scss'],
 })
 export class SupportedURLSPage implements OnInit  {
+    supportedURLS: [];
+
+    constructor(public dataService: DataService) { }
+
     ngOnInit() {
+        this.dataService.getSupportedURLs().subscribe((response) => {
+            if (typeof response == 'undefined')
+                 return;
+
+            this.supportedURLS = response;
+       },
+       error => {
+            //this.handleError(null, Response, error);
+       });
     }
+
+    // Used to prevent the entire DOM tree from being re-rendered every time that there is a change
+    trackByFn(index, item) {
+       return index; // or item.id
+    } 
 }
